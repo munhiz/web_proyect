36204458d8503f0ab461c39aa759b64c044b5781
Pablo González Muñiz <munhiz@Pablos-MacBook-Pro.local>
2018-06-07 13:31:24 +0200

    overlay menu done

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = '20%';
+    document.getElementById("myNav").style.width = '100%';
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = '0%';
+    document.getElementById("myNav").style.width = '0%';
+
+}
+
+
