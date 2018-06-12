0e5d50fcd3476f82d4f4fba5a0d9352c500f833b
Pablo González Muñiz <munhiz@Pablos-MacBook-Pro.local>
2018-06-12 14:02:26 +0200

    menu2

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,4 @@
 
-
-
 $( document ).ready(function() {
     console.log( "ready!" );
     //funcion scrolldown
@@ -12,16 +10,39 @@ $( document ).ready(function() {
     
 });
 
-    /* Open */
-    function openNav() {
-        document.getElementById("myNav").style.height = '20%';
-        document.getElementById("myNav").style.width = '100%';
-    }
-    
-    
-    /* Close */
-    function closeNav() {
-        document.getElementById("myNav").style.height = '0%';
-        document.getElementById("myNav").style.width = '0%';
-    
-    }
+
+
+//  Function SIDE MENU ----------------
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "125px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// function form contact ---------------
+
+//auto expand textarea
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
+
+function handCursor(){
+document.getElementById("hand").style.cursor = "pointer";
+}
+
+
+/* Set the width of the side navigation to 250px */
+function openNavCat() {
+    document.getElementById("mySidenav_cat").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNavCat() {
+    document.getElementById("mySidenav_cat").style.width = "0";
+}
